a9486959c943c43b4790d0a5c9b8a2c3348c3fde
cayotte alex <alex.cayotte@epitech.eu>
2019-04-04 12:50:41 +0200

    pushing background, and starting the UI

--- src/draw.c
+++ src/draw.c
@@ -19,22 +19,21 @@ void draw_background(window_t *win)
 window_t *draw_scene(window_t *win)
 {
     sfRenderWindow_clear(win->window, sfColor_fromRGB(25, 31, 38));
-    for (int i = 0; i < win->scene[win->actual_page].nb_button; i++) {
-        sfRenderWindow_drawRectangleShape(win->window,
-        win->scene[win->actual_page].button[i].rect, NULL);
-        sfRenderWindow_drawText(win->window,
-        win->scene[win->actual_page].button[i].text, NULL);
-    }
-    for (int i = 0; i < win->scene[win->actual_page].nb_text; i++) {
-        sfRenderWindow_drawText(win->window,
-        win->scene[win->actual_page].text[i].str, NULL);
-    }
-
     if (win->actual_page == GAME || win->actual_page == HOUSE) {
         draw_background(win);
     }
     for (int i = 0; i < win->scene[win->actual_page].nb_sprite; i++) {
         sfRenderWindow_drawSprite(win->window, win->scene[win->actual_page].sprite[i].sprite, NULL);
+    }
+    for (int i = 0; i < win->scene[win->actual_page].nb_button; i++) {
+        sfRenderWindow_drawRectangleShape(win->window,
+                                          win->scene[win->actual_page].button[i].rect, NULL);
+        sfRenderWindow_drawText(win->window,
+                                win->scene[win->actual_page].button[i].text, NULL);
+    }
+    for (int i = 0; i < win->scene[win->actual_page].nb_text; i++) {
+        sfRenderWindow_drawText(win->window,
+                                win->scene[win->actual_page].text[i].str, NULL);
     }
     if (win->actual_page == GAME || win->actual_page == HOUSE)
         sfRenderWindow_drawSprite(win->window, win->player->sprite->sprite, NULL);
--- src/init_menu.c
+++ src/init_menu.c
@@ -15,14 +15,17 @@ window_t *init_menu(window_t *win)
     sfVector2f pos_window;
     char **text = transform_2d("New game\nLoad game\nHow to play\nOptions\nExit\n");
 
+
     pos_window.x = (size_window.x - size.x) / 2;
     pos_window.y = (size_window.y - size.y) / 4;
     win->scene[MAINMENU].button = malloc(sizeof(button_t) * 5);
     win->scene[MAINMENU].text = malloc(sizeof(text_t) * 1);
+    win->scene[MAINMENU].sprite = malloc(sizeof(sprite_t) * 1);
     win->scene[MAINMENU].nb_text = 1;
     win->scene[MAINMENU].nb_button = 5;
-    win->scene[MAINMENU].nb_sprite = 0;
-    init_text(&win->scene[MAINMENU].text[0], "MY RPG", get_pos_float(pos_window.x + 90, 10));
+    win->scene[MAINMENU].nb_sprite = 1;
+    init_text(&win->scene[MAINMENU].text[0], "Tekzerk", get_pos_float(pos_window.x + 90, 10));
+    init_sprite(&win->scene[MAINMENU].sprite[0], "ressources/pack/background.png", get_pos_float(100, 0));
     for (int i = 0; i < win->scene[MAINMENU].nb_button; i++) {
         init_button(&win->scene[MAINMENU].button[i], pos_window, size);
         init_button_text(&win->scene[MAINMENU].button[i], text[i], get_pos_text_button(win->scene[MAINMENU].button[i], text[i]));
