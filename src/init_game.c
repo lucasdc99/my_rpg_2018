e75cc97b25096a296e37bad884137754b437a8c7
Thomas Renck <thomas.renck@epitech.eu>
2019-04-17 11:42:29 +0200

    adding tree sprites to be able to pass behind them

--- src/init_game.c
+++ src/init_game.c
@@ -13,13 +13,17 @@ window_t *init_game(window_t *win)
     sfVector2f size = get_pos_float(400, 100);
     int order_button[] = {REPRENDRE, QUITTER};
 
-    set_struct(win, 2, 0, 2);
+    set_struct(win, 2, 0, 6);
     set_next_buttons(&win->scene[CASTLE].button[0], win->rect_buttons, order_button[0]);
     init_button(&win->scene[CASTLE].button[0], get_pos_float(-200, -200), size, win->texture_button);
     set_next_buttons(&win->scene[CASTLE].button[1], win->rect_buttons, order_button[1]);
     init_button(&win->scene[CASTLE].button[1], get_pos_float(-200, -200), size, win->texture_button);
     init_sprite(&win->scene[CASTLE].sprite[0], "ressources/castle.png", get_pos_float(0, 0));
     init_sprite(&win->scene[CASTLE].sprite[1], "ressources/pack/rpg-pack/decorations/sword.png", get_pos_float(400, 400));
+    init_sprite(&win->scene[CASTLE].sprite[2], "ressources/tree.png", get_pos_float(304, 560));
+    init_sprite(&win->scene[CASTLE].sprite[3], "ressources/tree.png", get_pos_float(80, 458));
+    init_sprite(&win->scene[CASTLE].sprite[4], "ressources/tree.png", get_pos_float(1350, 508));
+    init_sprite(&win->scene[CASTLE].sprite[5], "ressources/tree2.png", get_pos_float(1520, 500));
     sfSprite_setScale(win->scene[CASTLE].sprite[1].sprite, get_pos_float(2, 2));
     win->scene[CASTLE].button[0].callback = &quit_pause;
     win->scene[CASTLE].button[1].callback = &main_menu;
