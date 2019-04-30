c2c15de627ccb7088296d8c406a15e5f71d46b8d
alex cayotte <alex.cayotte@epitech.eu>
2019-04-30 16:14:31 +0200

    [DOIN] depth of the sprites final boss map

--- src/init_boss.c
+++ src/init_boss.c
@@ -27,16 +27,24 @@ static void init_buttons(window_t *win)
 
 window_t *init_boss(window_t *win)
 {
-    set_struct(win, 2, 0, 2);
+    set_struct(win, 2, 0, 8);
     init_sprite(&win->scene[BOSS].sprite[0], "ressources/final_boss.png",
     get_pos_float(600, 0));
     init_sprite(&win->scene[BOSS].sprite[1],
     "ressources/images/sprites/Glenys-the-Demonswordsman.png",
     get_pos_float(920, 80));
+    init_sprite(&win->scene[BOSS].sprite[2], "ressources/sprite_contraste/statue.png", get_pos_float(582, 372));
+    init_sprite(&win->scene[BOSS].sprite[3], "ressources/sprite_contraste/statue_shield.png", get_pos_float(590, 621));
+    init_sprite(&win->scene[BOSS].sprite[4], "ressources/sprite_contraste/statue.png", get_pos_float(585, 830));
+    init_sprite(&win->scene[BOSS].sprite[5], "ressources/sprite_contraste/statue_shield.png", get_pos_float(1205, 376));
+    init_sprite(&win->scene[BOSS].sprite[6], "ressources/sprite_contraste/statue.png", get_pos_float(1200, 625));
+    init_sprite(&win->scene[BOSS].sprite[7], "ressources/sprite_contraste/statue_shield.png", get_pos_float(1202, 833));
     init_buttons(win);
     sfSprite_setScale(win->scene[BOSS].sprite[1].sprite, get_pos_float(3, 3));
     sfSprite_setTextureRect(win->scene[BOSS].sprite[1].sprite,
     get_rect(12, 9, 32, 32));
     win->scene[BOSS].sprite[0].depth = -1;
+    for (int i = 2; i < win->scene[BOSS].nb_sprite; i++)
+        win->scene[BOSS].sprite[i].depth = 1;
     return (win);
 }
